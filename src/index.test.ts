f3157c6650b532a42428128c8e3c4f83226a7f86
u150239 <Dominik.Bruhn@signal-iduna.de>
2022-07-05 14:25:45 +0200

    oijio

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,6 @@
 import {fixture, html} from '@open-wc/testing';
 
 test('html element check.', async () => {
-  const divElement : HTMLDivElement = await fixture(html`<div></div>`);
+  const divElement: HTMLDivElement = await fixture(html`<div></div>`);
   expect(divElement).toBeTruthy();
 });
