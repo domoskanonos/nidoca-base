da1cdfee6357fc48bb4402c3c3c43cfb749a512a
Dominik Bruhn <Dominik.Bruhn@signal-iduna.de>
2022-07-05 16:53:00 +0200

    Anpassungen

--- webpack.js
+++ webpack.js
@@ -3,36 +3,36 @@ import CopyPlugin from 'copy-webpack-plugin';
 import path from 'path';
 
 export default {
-   entry: './src/index.ts',
-   module: {
-      rules: [
-         {
-            test: /\.ts$/,
-            use: {loader: 'ts-loader', options: {transpileOnly: true}},
-         },
-      ],
-   },
-   resolve: {
-      extensions: ['.ts', '.js'],
-   },
-   optimization: {
-      minimize: true,
-   },
-   performance: {
-      maxEntrypointSize: 700000,
-   },
-   mode: 'production',
-   output: {
-      path: path.resolve().concat('/public'),
-      filename: 'nidoca-base.js',
-   },
-   plugins: [
-      new HtmlWebpackPlugin({
-         filename: 'index.html',
-         template: './src/index.html',
-      }),
-      new CopyPlugin({
-         patterns: [{from: 'src/main.css', to: 'main.css'}],
-      }),
-   ],
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: {loader: 'ts-loader', options: {transpileOnly: true}},
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  optimization: {
+    minimize: true,
+  },
+  performance: {
+    maxEntrypointSize: 700000,
+  },
+  mode: 'production',
+  output: {
+    path: path.resolve().concat('/public'),
+    filename: 'nidoca-base.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [{from: 'src/main.css', to: 'main.css'}],
+    }),
+  ],
 };
--- webpack.local.js
+++ webpack.local.js
@@ -2,33 +2,33 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import CopyPlugin from 'copy-webpack-plugin';
 
 export default {
-   target: 'web',
-   entry: {
-      main: './src/index.ts',
-   },
-   devServer: {
-      port: 8080,
-   },
-   resolve: {
-      extensions: ['.ts', '.js', 'css'],
-   },
-   performance: {
-      maxEntrypointSize: 700000,
-   },
-   mode: 'development',
-   devtool: 'inline-source-map',
-   module: {
-      rules: [
-         {
-            test: /\.ts$/,
-            use: {loader: 'ts-loader', options: {transpileOnly: true}},
-         },
-      ],
-   },
-   plugins: [
-      new HtmlWebpackPlugin({template: './src/index.html'}),
-      new CopyPlugin({
-         patterns: [{from: 'src/main.css', to: 'main.css'}],
-      }),
-   ],
+  target: 'web',
+  entry: {
+    main: './src/index.ts',
+  },
+  devServer: {
+    port: 8080,
+  },
+  resolve: {
+    extensions: ['.ts', '.js', 'css'],
+  },
+  performance: {
+    maxEntrypointSize: 700000,
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: {loader: 'ts-loader', options: {transpileOnly: true}},
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './src/index.html'}),
+    new CopyPlugin({
+      patterns: [{from: 'src/main.css', to: 'main.css'}],
+    }),
+  ],
 };
